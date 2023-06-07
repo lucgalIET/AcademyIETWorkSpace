d148f7c4d7f166344c6dd2d3151f7e51a20ce58b
GianmarcoGrimaldi <gianmarcogrimaldi.gg@gmail.com>
2023-06-07 11:54:43 +0200

    modifiche agli esercizi in classe

---
+++ Gianmarco/EserciziInClasse/Esercizio2FlussieFile.java
@@ -0,0 +1,23 @@
+package EserciziCasa_04_06_23;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Esercizio2FlussieFile {
+    public static void main(String[] args) {
+
+        try {
+            InputStream inputStream = new FileInputStream("numeri.bin");
+            int dato = inputStream.read();
+            while (dato != -1) {
+                System.out.print(dato);
+                dato = inputStream.read();
+            }
+            inputStream.close();
+
+        } catch (IOException exception) {
+            throw new RuntimeException();
+        }
+    }
+}
