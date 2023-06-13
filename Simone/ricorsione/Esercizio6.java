3643f5693e63ba43cd2cc61ed1ce535934d3529e
SimoneS2000 <simones143@gmail.com>
2023-06-13 17:07:37 +0200

    Esercizi ricorsione

---
+++ Simone/ricorsione/Esercizio6.java
@@ -0,0 +1,11 @@
+package EsercizioPianeti.ricorsione;
+/*
+Calcolo del numero di permutazioni
+Scrivi una funzione ricorsiva in Java che calcoli il numero di permutazioni di un insieme di n elementi.
+Una permutazione è un riarrangiamento degli elementi senza ripetizioni.
+Ad esempio, le permutazioni dell'insieme {1, 2, 3} sono: {1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2}, {3, 2, 1}.
+ */
+public class Esercizio6 {
+
+
+}
