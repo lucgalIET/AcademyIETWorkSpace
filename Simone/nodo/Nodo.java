8c7753fa557663ecfbe28b6815208129b4ffd7d3
SimoneS2000 <simones143@gmail.com>
2023-06-14 13:32:45 +0200

    thread

---
+++ Simone/nodo/Nodo.java
@@ -0,0 +1,27 @@
+package EsercizioPianeti.nodo;
+
+public class Nodo<T> {
+    private int valore;
+    private Nodo<T> nodoSuccessivo;
+
+    public Nodo(int valore) {
+        this.valore = valore;
+        this.nodoSuccessivo = null;
+    }
+
+    public int getValore() {
+        return valore;
+    }
+
+    public void setValore(int valore) {
+        this.valore = valore;
+    }
+
+    public Nodo<T> getNodoSuccessivo() {
+        return nodoSuccessivo;
+    }
+
+    public void setNodoSuccessivo(Nodo<T> nodoSuccessivo) {
+        this.nodoSuccessivo = nodoSuccessivo;
+    }
+}
