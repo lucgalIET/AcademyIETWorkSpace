0471f443e03411a0024a27d484b25c8c953e72e2
EnderModZe <manuel.arquati@gmail.com>
2023-06-14 09:25:14 +0200

    Esercizi ricorsione

---
+++ eserciziacasa/eserciziricorsione/Esercizio6.java
@@ -0,0 +1,30 @@
+package eserciziacasa.eserciziricorsione;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Esercizio6 {
+    static Set<String> stringSet = new TreeSet<>();
+    public static void main(String[] args) {
+        String stringa = "abcd";
+        sottoMassiRicorsiva(stringa);
+        System.out.println(stringSet);
+    }
+
+    public static void sottoMassi(String s) {
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                System.out.println(s.substring(i, j));
+            }
+        }
+    }
+
+    public static void sottoMassiRicorsiva(String s) {
+        if(s.isEmpty()) return;
+        stringSet.add(s);
+        sottoMassiRicorsiva(s.substring(1));
+        sottoMassiRicorsiva(s.substring(0,s.length()-1));
+    }
+}
