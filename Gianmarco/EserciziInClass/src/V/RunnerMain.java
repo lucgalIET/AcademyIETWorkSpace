dd866de207b67507304ac91467fd064ab02bcf2a
GianmarcoGrimaldi <gianmarcogrimaldi.gg@gmail.com>
2023-06-15 13:19:55 +0200

    esercizi

---
+++ Gianmarco/EserciziInClass/src/V/RunnerMain.java
@@ -0,0 +1,34 @@
+package V;
+
+import java.util.concurrent.Semaphore;
+
+/*
+Scrivi un programma in Java che simuli una corsa di 100 metri tra 2 squadre, A e B.
+Ogni squadra è composta da 4 corridori. Ogni corridore deve attraversare una sezione
+critica rappresentata da un tratto di 25 metri.
+
+ */
+public class RunnerMain implements Runnable {
+    private static Semaphore semaphore1 = new Semaphore(1);
+    private int metriPercorsiA;
+    private int metriPercorsiB;
+    private String team;
+
+    public RunnerMain() {
+        this.metriPercorsiA = 0;
+        this.metriPercorsiB = 0;
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+
+
+    public static void main(String[] args) {
+    Thread[] threadA1=new Thread[8];
+
+    }
+}
