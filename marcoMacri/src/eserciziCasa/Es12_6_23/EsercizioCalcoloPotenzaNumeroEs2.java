7fcc9eb2417a407670ddeb418413c0c387a05f1a
mak-1 <38510654+mak-1@users.noreply.github.com>
2023-06-15 09:10:03 +0200

    Esercizi casa 14-06-23

---
+++ marcoMacri/src/eserciziCasa/Es12_6_23/EsercizioCalcoloPotenzaNumeroEs2.java
@@ -0,0 +1,16 @@
+package marcoMacri.src.eserciziCasa.Es12_6_23;
+
+public class EsercizioCalcoloPotenzaNumeroEs2 {
+    public static void main(String[] args) {
+
+            int base = 5, esponente = 3;
+            System.out.println ("La potenza calcolata è " + potenza(base, esponente));
+        }
+
+        static int potenza(int base, int esponente)
+        {
+            if (esponente == 0)     //caso base
+                return 1;
+            return (base * potenza(base, esponente - 1));
+        }
+    }
