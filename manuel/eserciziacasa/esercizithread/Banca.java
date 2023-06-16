624a5555075ea4395925e1a606f0dfb5b3fb4a49
EnderModZe <manuel.arquati@gmail.com>
2023-06-16 17:30:38 +0200

    Esercizi risolti

--- manuel/eserciziacasa/esercizithread/Banca.java
+++ manuel/eserciziacasa/esercizithread/Banca.java
@@ -1,50 +1,42 @@
 package manuel.eserciziacasa.esercizithread;
 
-public class Banca {
-    private double conto = 0;
+import java.util.Scanner;
 
+public class Banca implements Runnable {
+    private double conto = 0;
     public Banca() {
     }
 
-    public synchronized void deposito(double soldi){
-        if(soldi <= 0) return;
-        conto += soldi;
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+    @Override
+    synchronized public void run() {
+        System.out.println(Thread.currentThread().getName());
+        System.out.println("CONTO ATTUALE: "+conto);
+        Scanner myScan = new Scanner(System.in);
+        System.out.println("Seleziona P per prelevare o D per depositare");
+        String s = myScan.next();
+        if(s.equals("P")){
+            System.out.println("Quanto vuoi prelevare? ");
+            double soldi = myScan.nextDouble();
+            if(soldi <= 0) return;
+            else if(soldi > conto) {
+                System.out.println("Denaro insufficiente");
+                return;
+            }
+            conto -= soldi;
+        } else if (s.equals("D")) {
+            System.out.println("Quanto vuoi depositare? ");
+            double soldi = myScan.nextDouble();
+            if(soldi <= 0) return;
+            conto += soldi;
         }
+
     }
-
-    public synchronized void prelievo(double soldi){
-        if(soldi <= 0) return;
-        else if(soldi > conto) {
-            System.out.println("Denaro insufficiente");
-            return;
-        }
-        conto -= soldi;
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
 
     public static void main(String[] args) {
         Banca banca = new Banca();
-        Thread thread1 = new Thread();
-        Thread thread2 = new Thread();
-
-
+        Thread thread1 = new Thread(banca);
+        Thread thread2 = new Thread(banca);
+        thread1.start();
+        thread2.start();
     }
-
-
-
-
-//    @Override
-//    synchronized public void run() {
-//
-//    }
 }
