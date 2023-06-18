39c1d94cd08514fe703765bb0a647a2911bbc9ff
Ciro Cancello <cirocancello75@gmail.com>
2023-06-18 17:40:19 +0200

    Create Banca.java

---
+++ Ciro/thread/esercizio2/Banca.java
@@ -0,0 +1,46 @@
+package Ciro.thread.esercizio2;
+
+
+import java.util.Scanner;
+
+public class Banca implements Runnable {
+    private double conto = 0;
+    public Banca() {
+    }
+
+    @Override
+    synchronized public void run() {
+        System.out.println(Thread.currentThread().getName());
+        System.out.println("CONTO ATTUALE: "+conto);
+        Scanner myScan = new Scanner(System.in);
+        System.out.println("Seleziona P per prelevare o D per depositare");
+        String s = myScan.next();
+        if(s.equals("P")){
+            System.out.println("Quanto vuoi prelevare? ");
+            double soldi = myScan.nextDouble();
+            if(soldi <= 0) return;
+            else if(soldi > conto) {
+                System.out.println("Denaro insufficiente");
+                return;
+            }
+            conto -= soldi;
+        } else if (s.equals("D")) {
+            System.out.println("Quanto vuoi depositare? ");
+            double soldi = myScan.nextDouble();
+            if(soldi <= 0) return;
+            conto += soldi;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Banca banca = new Banca();
+
+        for(int i = 0; i < 10; i++){
+            Thread thread1 = new Thread(banca);
+            Thread thread2 = new Thread(banca);
+            thread1.start();
+            thread2.start();
+        }
+    }
+}
