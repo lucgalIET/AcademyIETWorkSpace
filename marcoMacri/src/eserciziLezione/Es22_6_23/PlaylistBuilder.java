465d304507d160b03f1cd44f2e579efe0dec04a4
mak-1 <38510654+mak-1@users.noreply.github.com>
2023-06-22 10:22:14 +0200

    Esercizio Lezione 22-6-23 design pattern builder

---
+++ marcoMacri/src/eserciziLezione/Es22_6_23/PlaylistBuilder.java
@@ -0,0 +1,15 @@
+package marcoMacri.src.eserciziLezione.Es22_6_23;
+
+import java.util.List;
+
+//Esercizio n
+//        Sviluppare un'applicazione per la creazione di un sistema di gestione di playlist musicali.
+//        Ogni playlist può contenere diverse canzoni con attributi come il titolo, l'artista, l'album, la durata, ecc.
+//        La classe 'x' dovrebbe fornire metodi per aggiungere canzoni alla playlist, impostare il titolo, ordinare le canzoni, ecc.
+public interface PlaylistBuilder {
+    void setTitoloPlaylist(String titolo);
+    List<Canzone> ordinaCanzoni();
+    void aggiungiCanzone(Canzone canzone);
+    Playlist getPlaylist();
+
+}
---
+++ marcoMacri/src/eserciziLezione/Es22_6_23/PlaylistMain.java
@@ -0,0 +1,20 @@
+package marcoMacri.src.eserciziLezione.Es22_6_23;
+
+//Esercizio n
+//        Sviluppare un'applicazione per la creazione di un sistema di gestione di playlist musicali.
+//        Ogni playlist può contenere diverse canzoni con attributi come il titolo, l'artista, l'album, la durata, ecc.
+//        La classe 'x' dovrebbe fornire metodi per aggiungere canzoni alla playlist, impostare il titolo, ordinare le canzoni, ecc.
+public class PlaylistMain {
+    public static void main(String[] args) {
+        CanzoneBuilder canzoneBuilder= new Canzone();
+        CanzoneDirector canzoneDirector= new CanzoneDirector(canzoneBuilder);
+        Canzone canzone1 = canzoneDirector.constructorCanzone(new Canzone("artista1","album1",200,"titolo"));
+
+        PlaylistBuilder playlistBuilder = new Playlist();
+        PlaylistDirector playlistDirector = new PlaylistDirector(playlistBuilder);
+        Playlist playlistFantastica = playlistDirector.constructorPlaylist(canzone1);
+
+        System.out.println(playlistFantastica);
+    }
+
+}
