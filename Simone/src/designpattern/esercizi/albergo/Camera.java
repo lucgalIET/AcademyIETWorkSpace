e4252a69dd8a40735a84fdbc9bf64f915b82b8aa
Ciro Cancello <cirocancello75@gmail.com>
2023-06-23 08:11:27 +0200

    commit

--- Simone/src/designpattern/esercizi/albergo/Camera.java
+++
@@ -1,6 +0,0 @@
-package designpattern.esercizi.albergo;
-
-public interface Camera {
-  void mostraDettagliCamera();
-
-}
--- Simone/src/designpattern/esercizi/pizzeria/QuattroFormaggiCreator.java
+++
@@ -1,8 +0,0 @@
-package designpattern.esercizi.pizzeria;
-
-public class QuattroFormaggiCreator extends PizzaCreator{
-    @Override
-    public Pizza creaPizza() {
-        return new QuattroFormaggi("Gorgonzola, Fontina, Mozzarella, Parmigiano, Olio");
-    }
-}
