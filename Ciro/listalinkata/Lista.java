7c4129e510dfa461057c600e38480eee4f6abbeb
Ciro Cancello <cirocancello75@gmail.com>
2023-06-28 08:43:56 +0200

    lista linkata

---
+++ Ciro/listalinkata/Lista.java
@@ -0,0 +1,11 @@
+package Ciro.listalinkata;
+
+public interface Lista {
+
+    void aggiungiElemento(int elem);
+    void elimineElemento();
+    int testa();
+    void stampaLista();
+
+    void inserisciElementi(Lista lista);
+}
